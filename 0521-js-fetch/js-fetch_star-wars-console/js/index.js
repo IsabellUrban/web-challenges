3750d71c6d7c01c1061dc140588392dd0156d06e
Isabell Urban <mail@isabellurban.com>
2024-05-21 12:56:40 +0200

    add todays challenges

---
+++ 0521-js-fetch/js-fetch_star-wars-console/js/index.js
@@ -0,0 +1,14 @@
+console.clear();
+
+const url = "https://swapi.py4e.com/api/people";
+
+async function fetchData() {
+  const response = await fetch(url);
+  const starWars = await response.json();
+
+  console.log(starWars);
+  console.log(starWars.results[2].eye_color);
+  console.log(starWars.results[5].name);
+}
+
+fetchData();
