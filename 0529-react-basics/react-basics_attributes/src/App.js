20e19175d4dde112718f3764df4c87ee28d51460
Isabell Urban <mail@isabellurban.com>
2024-05-29 12:22:01 +0200

    add changes

--- 0529-react-basics/react-basics_attributes/src/App.js
+++ 0529-react-basics/react-basics_attributes/src/App.js
@@ -10,6 +10,7 @@ function Article() {
     <article className="article">
       <h2 className="article__title">I am a headline!</h2>
       <label htmlFor="input">I am a label.</label>
+      <input id="input"></input>
       <a
         className="article__link"
         href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/article"
